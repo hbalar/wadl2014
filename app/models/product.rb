4abde806ddee8856910d59d9b20c59458b83e61f
hardik101 <hardikbalar101@gmail.com>
2014-02-10 14:05:26 +0100

    Depot complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  default_scope :order => :title
+	
+	validates :title , :description, :image_url , :price ,  :presence => true 
+	validates :price , :numericality => {:greater_than_or_equal_to => 0.01 }
+	validates :title , :uniqueness => true
+	validates :image_url , :format => {
+
+				:with    => %r{\.(png|jpg|gif)$}i ,
+				:message => 'Must be a URL for JPG ,PNG or GIF image'
+			}
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
+
     @product = products(:one)
+     
+    @update = {
+
+      :title => 'The alchemist',
+      :description => 'The ruby philosophy',
+      :image_url => 'lorem.jpg',
+      :price => 12.2
+
+
+    }
   end
 
   test "should get index" do
@@ -18,7 +29,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, :product => @update 
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +46,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, id: @product, :product => @update
     assert_redirected_to product_path(assigns(:product))
   end
 
